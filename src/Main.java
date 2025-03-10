529b92db7448d6585cb7d8db5990356538d9a198
playkoround <seasouphan@gmail.com>
2025-03-10 17:13:09 +0900

    LV1 choose the menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -12,10 +10,33 @@ public class Main {
                 2. Smoked burger | W 8.9 | Flamed-grilled beef patty topped with smoked bacon
                 3. Cheeseburger  | W 6.9 | Flamed-grilled beef patty topped with melted cheese and tomato
                 4. Hamburger     | W 5.4 | Two flamed-grilled beef patties with lettuce
-                0. 종료""");
+                0. Exit""");
 
         // 스캐너 객체 생성
         Scanner sc = new Scanner(System.in);
 
-            }
+        // 메뉴 선택
+        System.out.println("Choose your choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("You ordered: SyBurger");
+                break;
+            case 2:
+                System.out.println("You ordered: smoked burger");
+                break;
+            case 3:
+                System.out.println("You ordered: cheeseburger");
+                break;
+            case 4:
+                System.out.println("You ordered: hamburger");
+                break;
+            case 0:
+                break;
+        }
+
+
+
+    }
 }
