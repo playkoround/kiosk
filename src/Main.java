5d7e17b2b56cb5c0b7c89024bf2934ace8e2e31e
playkoround <seasouphan@gmail.com>
2025-03-10 14:44:53 +0900

    LV1 Print the menu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        // 메뉴 자동 출력, 코드 가독성을 위해 """ """을 이용
+        System.out.println("""
+                [ Sy's Burger ]
+                1. SyBurger      | W 6.9 | 100% Angus Beef flame-grilled topped with fresh Rocket
+                2. Smoked burger | W 8.9 | Flamed-grilled beef patty topped with smoked bacon
+                3. Cheeseburger  | W 6.9 | Flamed-grilled beef patty topped with melted cheese and tomato
+                4. Hamburger     | W 5.4 | Two flamed-grilled beef patties with lettuce
+                0. 종료""");
+
+        // 스캐너 객체 생성
+        Scanner sc = new Scanner(System.in);
+
+            }
 }
