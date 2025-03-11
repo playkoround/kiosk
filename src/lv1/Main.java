efa4cbf9e3ea83181700a3d85e0993c05de2a5df
playkoround <seasouphan@gmail.com>
2025-03-11 16:58:07 +0900

    Refactor: if문에 continue 추가 잘못된 숫자 입력 시, 밑의 switch문까지 읽을 필요가 없게 리팩토링함

--- src/lv1/Main.java
+++ src/lv1/Main.java
@@ -16,18 +16,25 @@ public class Main {
                 4. Hamburger     | W 5.4 | Two flamed-grilled beef patties with lettuce
                 0. Exit""");
 
-        // while문을 위한 불리안 변수 생성 (유효한 입력 들어올 때까지 반복)
-        boolean flag = true;
+        // while문을 위한 불리안 변수 생성 (유효한 입력값이 들어올 때까지 반복)
+        boolean isValidInt = false;
 
-        while (flag) {
+        while (!isValidInt) {
+
+            // 유효한 입력값이 안들어오는 예외 처리
             try {
                 System.out.println("Choose your choice: ");
+
+                // 입력값 받기
                 int choice = sc.nextInt();
-                // 입력값 0~4인지 확인
+
+                // 입력값이 숫자 0~4인지 확인
                 if (choice >= 0 && choice <= 4) {
-                    flag = false; // 올바른 입력이면 반복 종료
+                    // 올바른 숫자이면 반복 종료
+                    isValidInt = true;
                 } else {
                     System.out.println("Please enter a number between 0 and 4");
+                    continue; //continue로 밑에 스위치까지 안 읽고, 바로 입력값 받게 만들기
                 }
                     switch (choice) {
                         case 1:
@@ -46,6 +53,7 @@ public class Main {
                             break;
                     }
             } catch (InputMismatchException e) {
+                // 입력값이 숫자가 아닌 예외 처리
                 System.out.println("Please enter a number between 0 and 4");
                 sc.nextLine(); // 개행문자 > 버퍼에는 계속 입력값이 남아있음 > 그래서 nextLine이 엔터(컴퓨터 = \n)를 통해 없앤다.
             }
