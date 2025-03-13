df04b13bddf77f3f98fb0cf591500471609c2980
playkoround <seasouphan@gmail.com>
2025-03-13 17:59:50 +0900

    Rename: Kiosk.java에 있던 menu관련 것들을 Menu.java로 이동 현재 Kiosk 클라스 내, 수많은 menuItems. ~~ 이 작동하지 않는다. Fixes: #20 ~

--- src/lv4/Menu.java
+++ src/lv4/Menu.java
@@ -1,5 +1,27 @@
 package lv4;
 
+import lv3.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+    private List<lv3.MenuItem> menuItems = new ArrayList<>();
+
+
+    // MenuItem 인스턴스화
+    lv3.MenuItem syBurger = new lv3.MenuItem("Sy Burger", 7900, "100% Angus Beef flame-grilled topped with fresh Rocket");
+    lv3.MenuItem smokedBurger = new lv3.MenuItem("Smoked Burger", 8900, "Flamed-grilled beef patty topped with smoked bacon");
+    lv3.MenuItem cheeseBurger = new lv3.MenuItem("Cheese Burger", 6900, "Flamed-grilled beef patty topped with melted cheese and tomato");
+    lv3.MenuItem hamburger = new MenuItem("Hamburger", 6900, "Two flamed-grilled beef patties with lettuce");
+
+    public void menu() {
+        //     MenuItem을 담을 menuItems에 리스트 인터페이스 구현한 ArrayList 인스턴스화하여 할당
+        //     메서드 안에 있어야 활용이 가능하니, start 메서드에 우선 넣어주었다.
+        menuItems.add(syBurger);
+        menuItems.add(smokedBurger);
+        menuItems.add(cheeseBurger);
+        menuItems.add(hamburger);
+    }
 
 }
